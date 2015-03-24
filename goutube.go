a48dfbb3c9f9681590e6f910588df2c39d6a9208
Sreejith <sreejith.r44@gmail.com>
2015-03-24 12:50:55 +0530

    Changes which would ultimately get discarded

--- goutube.go
+++ goutube.go
@@ -1,20 +1,19 @@
-package goutube
+package main
 
-type Video interface {
-	Link string
+import (
+	"fmt"
+)
+
+// TODO: Check if valid Youtube url. If not (minified urls), try following
+
+/**
+video <-goutube.Youtube(link)
+args := video.args
+downloadLink := video.
+*/
+
+func main() {
+ 	args := GetYoutubeConfigArgs("https://www.youtube.com/watch?v=iZq3i94mSsQ")
+	videos := ExtractVideoFormats(args)
+	fmt.Println(videos[0].URL)
 }
-
-type Result struct {
-	Mp4 Video
-}
-
-func Youtube(url string) <-chan Result {
-	// Youtube returns a channel which pumps a `Result` whenever it is ready.
-	// ...
-	result := make(chan Result)
-
-	// TODO: Check if valid Youtube url. If not (minified urls), try following
-
-	
-}
-
