cf419cc58feb1a175090863c33ce73f0e093f0d1
Sreejith <sreejith.r44@gmail.com>
2015-05-14 13:35:11 +0530

    Lot of test code

--- goutube.go
+++ goutube.go
@@ -4,16 +4,21 @@ import (
 	"fmt"
 )
 
-// TODO: Check if valid Youtube url. If not (minified urls), try following
+// TODO: Check if valid Youtube url. If not (minified urls), try following them
 
 /**
-video <-goutube.Youtube(link)
+video <- goutube.Youtube(link)
 args := video.args
 downloadLink := video.
 */
-
 func main() {
- 	args := GetYoutubeConfigArgs("https://www.youtube.com/watch?v=iZq3i94mSsQ")
-	videos := ExtractVideoFormats(args)
-	fmt.Println(videos[0].URL)
+ 	args, err := GetYoutubeConfigArgs("https://www.youtube.com/watch?v=iZq3i94mSsQ")
+	if err != nil {
+		return
+	}
+	results, _ := GetResults(args)
+
+	for _, result := range results {
+		fmt.Println(result.Format.Type)
+	}
 }
